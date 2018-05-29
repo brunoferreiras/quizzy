fb1d55a204a1a00930bb51d75566467c64c02196
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-05-29 14:55:19 -0300

    Remove api and moved mobile

--- api/src/auth/config.js
+++
@@ -1,8 +0,0 @@
-import { ExtractJwt } from 'passport-jwt'
-import { config } from 'dotenv'
-config()
-
-export default {
-    secretOrKey: process.env.JWT_SECRET_KEY,
-    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('JWT')
-}
